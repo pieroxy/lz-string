537ca20bf90df0cd9ea829e58e8813e05162f7bd
pieroxy <pieroxy@pieroxy.net>
2013-08-22 23:23:44 +0200

    Version 1.3.2 was completely bugged
    I still have no idea why, so if anyone want to to explain, just look at the previous commit...

--- libs/lz-string-1.3.3.js
+++ libs/lz-string-1.3.3.js
@@ -6,7 +6,7 @@
 // For more information, the home page:
 // http://pieroxy.net/blog/pages/lz-string/testing.html
 //
-// LZ-based compression algorithm, version 1.3.2
+// LZ-based compression algorithm, version 1.3.3
 var LZString = {
   
   
