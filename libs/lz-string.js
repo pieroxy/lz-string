4e4cea10f008cbe475674a53fd876e189050c1b5
JobLeonard <j.l.vanderzwan@gmail.com>
2017-07-01 20:23:16 +0200

    Shortened decompress

--- libs/lz-string.js
+++ libs/lz-string.js
@@ -309,162 +309,111 @@ var LZString = {
     return LZString._decompress(compressed.length, 32768, function(index) { return compressed[index].charCodeAt(0); });  },
 
   _decompress: function (length, resetValue, getNextValue) {
-    var dictionary = [],
-        next,
+    var dictionary = [0, 1, 2],
         enlargeIn = 4,
         dictSize = 4,
         numBits = 3,
         entry = "",
         result = [],
-        i,
-        w,
-        bits, resb, maxpower, power,
-        c,
-        data = {val:getNextValue(0), position:resetValue, index:1};
+        w = "",
+        bits = 0,
+        resb = 0,
+        maxpower = 2,
+        power = 0,
+        c = "",
+        data_val = getNextValue(0),
+        data_position = resetValue,
+        data_index = 1;
 
-    for (i = 0; i < 3; i += 1) {
-      dictionary[i] = i;
+    while (power!=maxpower) {
+      resb = data_val & data_position;
+      data_position >>= 1;
+      if (data_position == 0) {
+        data_position = resetValue;
+        data_val = getNextValue(data_index++);
+      }
+      bits |= (resb>0 ? 1 : 0) << power++;
     }
+
+    if (bits == 2){
+      return ""
+    }
+    //Math.pow(2,8 + 8*bits);
+    maxpower = bits*8+8;
 
     bits = 0;
-    maxpower = Math.pow(2,2);
-    power=1;
+    power = 0;
     while (power!=maxpower) {
-      resb = data.val & data.position;
-      data.position >>= 1;
-      if (data.position == 0) {
-        data.position = resetValue;
-        data.val = getNextValue(data.index++);
+      resb = data_val & data_position;
+      data_position >>= 1;
+      if (data_position == 0) {
+        data_position = resetValue;
+        data_val = getNextValue(data_index++);
       }
-      bits |= (resb>0 ? 1 : 0) * power;
-      power <<= 1;
+      bits |= (resb>0 ? 1 : 0) << power++;
     }
-
-    switch (next = bits) {
-      case 0:
-          bits = 0;
-          maxpower = Math.pow(2,8);
-          power=1;
-          while (power!=maxpower) {
-            resb = data.val & data.position;
-            data.position >>= 1;
-            if (data.position == 0) {
-              data.position = resetValue;
-              data.val = getNextValue(data.index++);
-            }
-            bits |= (resb>0 ? 1 : 0) * power;
-            power <<= 1;
-          }
-        c = f(bits);
-        break;
-      case 1:
-          bits = 0;
-          maxpower = Math.pow(2,16);
-          power=1;
-          while (power!=maxpower) {
-            resb = data.val & data.position;
-            data.position >>= 1;
-            if (data.position == 0) {
-              data.position = resetValue;
-              data.val = getNextValue(data.index++);
-            }
-            bits |= (resb>0 ? 1 : 0) * power;
-            power <<= 1;
-          }
-        c = f(bits);
-        break;
-      case 2:
-        return "";
-    }
+    c = f(bits);
     dictionary[3] = c;
     w = c;
     result.push(c);
+
     while (true) {
-      if (data.index > length) {
+      if (data_index > length) {
         return "";
       }
 
       bits = 0;
-      maxpower = Math.pow(2,numBits);
-      power=1;
+      maxpower = numBits;//Math.pow(2,numBits);
+      power = 0;
       while (power!=maxpower) {
-        resb = data.val & data.position;
-        data.position >>= 1;
-        if (data.position == 0) {
-          data.position = resetValue;
-          data.val = getNextValue(data.index++);
+        resb = data_val & data_position;
+        data_position >>= 1;
+        if (data_position == 0) {
+          data_position = resetValue;
+          data_val = getNextValue(data_index++);
         }
-        bits |= (resb>0 ? 1 : 0) * power;
-        power <<= 1;
+        bits |= (resb>0 ? 1 : 0) << power++;
       }
 
-      switch (c = bits) {
-        case 0:
-          bits = 0;
-          maxpower = Math.pow(2,8);
-          power=1;
-          while (power!=maxpower) {
-            resb = data.val & data.position;
-            data.position >>= 1;
-            if (data.position == 0) {
-              data.position = resetValue;
-              data.val = getNextValue(data.index++);
-            }
-            bits |= (resb>0 ? 1 : 0) * power;
-            power <<= 1;
+      if (bits == 0 || bits == 1){
+        power=0;
+        maxpower = (8+8*bits);//Math.pow(2,8 + 8*bits);
+        bits = 0;
+        while (power!=maxpower) {
+          resb = data_val & data_position;
+          data_position >>= 1;
+          if (data_position == 0) {
+            data_position = resetValue;
+            data_val = getNextValue(data_index++);
           }
-
-          dictionary[dictSize++] = f(bits);
-          c = dictSize-1;
-          enlargeIn--;
-          break;
-        case 1:
-          bits = 0;
-          maxpower = Math.pow(2,16);
-          power=1;
-          while (power!=maxpower) {
-            resb = data.val & data.position;
-            data.position >>= 1;
-            if (data.position == 0) {
-              data.position = resetValue;
-              data.val = getNextValue(data.index++);
-            }
-            bits |= (resb>0 ? 1 : 0) * power;
-            power <<= 1;
-          }
-          dictionary[dictSize++] = f(bits);
-          c = dictSize-1;
-          enlargeIn--;
-          break;
-        case 2:
-          return result.join('');
+          bits |= (resb>0 ? 1 : 0) << power++;
+        }
+        dictionary[dictSize] = f(bits);
+        bits = dictSize++;
+        if (--enlargeIn == 0) {
+          enlargeIn = Math.pow(2, numBits++);
+        }
+      } else if (bits == 2){
+        return result.join('');
       }
 
-      if (enlargeIn == 0) {
-        enlargeIn = Math.pow(2, numBits);
-        numBits++;
-      }
 
-      if (dictionary[c]) {
-        entry = dictionary[c];
+      if (dictionary[bits]) {
+        entry = dictionary[bits];
+      } else if (bits === dictSize) {
+        entry = w + w.charAt(0);
       } else {
-        if (c === dictSize) {
-          entry = w + w.charAt(0);
-        } else {
-          return null;
-        }
+        return null;
       }
       result.push(entry);
 
       // Add w+entry[0] to the dictionary.
       dictionary[dictSize++] = w + entry.charAt(0);
-      enlargeIn--;
 
       w = entry;
 
-      if (enlargeIn == 0) {
-        enlargeIn = Math.pow(2, numBits);
-        numBits++;
+      if (--enlargeIn == 0) {
+        enlargeIn = 1 << numBits++;
       }
 
     }
