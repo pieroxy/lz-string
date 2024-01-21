b8f68985cf9a2712831ece7d2fe09da0c4314074
Erin Rivas <me@erinrivas.com>
2024-01-20 19:07:02 -0600

    removing browser-specific elements from utils

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -3,12 +3,11 @@ type VERSIONS = "v2.0.0"
 export function deprecated(
     thing: string,
     version: VERSIONS,
-    opts?: { aggressive?: boolean; replacement?: string }
+    opts?: { replacement?: string }
 ): void {
     let notice = `LZString | ${thing} is deprecated as of: ${version}`
     if (opts?.replacement) {
         notice += ` - Please use ${opts.replacement} instead`
     }
     console.error(notice)
-    if (window && opts?.aggressive) alert(notice)
 }
